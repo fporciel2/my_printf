cf51fb7b106ced5ef0c3028082714fa2593e0a19
Federico Porciello <fporciel@e4r6p4.42roma.it>
2023-03-01 10:21:08 +0100

    Added the mother of parsing functions.

---
+++ 1_parsing/ft_is_format_spec.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_format_spec.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 09:56:46 by fporciel          #+#    #+#             */
+/*   Updated: 2023/03/01 10:20:13 by fporciel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int	ft_is_format_spec(const char *format, int i)
+{
+	i++;
+	if (format[i] == 37)
+		return (1);
+	else if (ft_has_pos_arg(format, i))
+		return (3);
+	else if (ft_has_flags(format, i))
+		return (2);
+	else if (ft_has_min_fieldwidth(format, i))
+		return (2);
+	else if (ft_has_precision(format, i))
+		return (2);
+	else if (ft_has_lengthmod(format, i))
+		return (2);
+	else if (ft_is_conv_spec(format[i]))
+		return (2);
+	else
+		return (0);
+}
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 09:24:41 by fporciel          #+#    #+#             */
-/*   Updated: 2023/03/01 09:26:54 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/03/01 09:56:26 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include "libft.h"
 
 int	ft_printf(const char *format, ...);
+int	ft_is_format_spec(const char *format, int i);
 
 #endif
