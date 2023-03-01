22fbc20566cc1e1edb9786408d089dd97bb41db5
Federico Porciello <fporciel@e4r6p4.42roma.it>
2023-03-01 17:42:41 +0100

    Updated the header file.

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 09:24:41 by fporciel          #+#    #+#             */
-/*   Updated: 2023/03/01 12:10:07 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/03/01 17:41:46 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@ int	ft_printf(const char *format, ...);
 int	ft_is_format_spec(const char *format, int i);
 int	ft_nonposit_conv(const char *format, int *i, va_list &ap1);
 int	ft_posit_conv(const char *format, int *i, va_list ap);
+int	ft_has_pos_arg(const char *format, int i);
+int	ft_has_flags(const char *format, int i);
 
 #endif
