769f2c548370fe2ef51c027ad5bee657a92ed916
Federico Porciello <fporciel@e4r6p4.42roma.it>
2023-03-01 17:15:32 +0100

    Added conversion fork.

--- 2_conversions/ft_nonposit_conv.c
+++ 2_conversions/ft_nonposit_conv.c
@@ -6,7 +6,7 @@
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 10:24:25 by fporciel          #+#    #+#             */
-/*   Updated: 2023/03/01 15:20:57 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/03/01 17:13:34 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int	ft_nonposit_conv(const char *format, int *i, va_list *ap1, va_list ap)
 
 	j = 0;
 	result = 0;
-	node = (*ap1).__ap;
-	result = ft_conversion(format, i, node);
-	(*ap1).__ap = node->__va_next;
+	node = (*ap1)->__ap;
+	result = ft_conversion(format, i, node, ap);
+	(*ap1)->__ap = node->__va_next;
 	free(node);
 	return (result);
 }
---
+++ 2_conversions/ft_posit_conv.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_posit_conv.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 15:26:18 by fporciel          #+#    #+#             */
+/*   Updated: 2023/03/01 17:14:04 by fporciel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+static __va_elem_t	*ft_goto_position(__va_elem_t **node, int positional_arg)
+{
+	while (positional_arg != 1)
+	{
+		(*node) = (*node)->__va_next;
+		positional_arg;
+	}
+	return (*node);
+}
+
+static int	ft_power(int base, int exp)
+{
+	int	power;
+
+	power = base;
+	if ((base == 0) && (exp != 0))
+		return (0);
+	if (exp == 0)
+		return (1);
+	if (exp == 1)
+		return (base);
+	if (exp == 2)
+		return (base * base);
+	while (exp > 1)
+	{
+		power = (power * base);
+		exp--;
+	}
+	return (power);
+}
+
+static int	ft_calculate_position(const char *format, int j)
+{
+	int	length;
+	int	num;
+	int	exp;
+
+	num = 0;
+	length = j;
+	while (format[j] != 36)
+		j++;
+	exp = ((j - length) - 1);
+	while (length < j)
+	{
+		num = num + ((format[length] - 48) * ft_power(10, exp));
+		exp--;
+		length++;
+	}
+	return (num);
+}
+
+int	ft_posit_conv(const char *format, int *i, va_list ap)
+{
+	int			j;
+	int			result;
+	int			positional_arg;
+	__va_elem_t	*node;
+
+	j = (*i) + 1;
+	node = ap->__ap;
+	positional_arg = ft_calculate_position(format, j);
+	node = ft_goto_position(&node, positional_arg);
+	result = ft_conversion(format, i, node, ap);
+	free(node);
+	return (result);
+}
