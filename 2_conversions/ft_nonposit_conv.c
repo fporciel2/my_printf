62558842572b3128d5545dc166b9b4482e8db200
Federico Porciello <fporciel@e4r6p4.42roma.it>
2023-03-01 15:14:28 +0100

    Completed the function for positional conversion

--- 2_conversions/ft_nonposit_conv.c
+++ 2_conversions/ft_nonposit_conv.c
@@ -6,17 +6,23 @@
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 10:24:25 by fporciel          #+#    #+#             */
-/*   Updated: 2023/03/01 11:45:35 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/03/01 14:57:51 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_nonposit_conv(const char *format, int *i, va_list ap1)
+int	ft_nonposit_conv(const char *format, int *i, va_list *ap1)
 {
-	int	result;
+	int			j;
+	int			result;
+	__va_elem_t	*node;
 
+	j = 0;
 	result = 0;
-	result = ft_conversion(format, i, ap1);
+	node = (*ap1).__ap;
+	result = ft_conversion(format, i, node);
+	(*ap1).__ap = node->__va_next;
+	free(node);
 	return (result);
 }
