bcc9acc9728f3f2171fa239096164ba52046b245
Federico Porciello <fporciel@e4r7p4.42roma.it>
2023-03-02 10:46:10 +0100

    Added length modifier's parser and corrected flags' parser.

--- 1_parsing/ft_has_lengthmod.c
+++ 1_parsing/ft_has_lengthmod.c
@@ -1,45 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_has_flags.c                                     :+:      :+:    :+:   */
+/*   ft_has_lengthmod.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/01 17:44:38 by fporciel          #+#    #+#             */
-/*   Updated: 2023/03/02 10:39:33 by fporciel         ###   ########.fr       */
+/*   Created: 2023/03/02 10:31:17 by fporciel          #+#    #+#             */
+/*   Updated: 2023/03/02 10:44:16 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static int	ft_has_flags2(const char *format, int j)
+static int	ft_has_lengthmod2(const char *format, int j)
 {
-	if (!(ft_isflag(format[j])))
+	if (!(ft_islengthmod(format[j])))
 		return (0);
 	else
 	{
-		while (ft_isflag(format[j]))
+		while (ft_islengthmod(format[j]))
 			j++;
-		return (ft_has_min_fieldwidth(format, j));
+		return (ft_is_conv_spec(format[j]));
 	}
 }
 
-int	ft_has_flags(const char *format, int i)
+int	ft_has_lengthmod(const char *format, int i)
 {
 	int	j;
 
 	j = i;
 	if (format[j - 1] != 37)
 	{
-		if (!(ft_isflag(format[j])))
-			return (ft_has_min_fieldwidth(format, j));
+		if (!(ft_islengthmod(format[j])))
+			return (ft_is_conv_spec(format[j]));
 		else
 		{
-			while (ft_isflag(format[j]))
+			while (ft_islengthmod(format[j]))
 				j++;
-			return (ft_has_min_fieldwidth(format, j));
+			return (ft_is_conv_spec(format[j]))
 		}
 	}
 	else
-		return (ft_has_flags2(format, j));
+		return (ft_has_lengthmod2(format, j));
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 09:24:41 by fporciel          #+#    #+#             */
-/*   Updated: 2023/03/02 10:00:34 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/03/02 10:30:48 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int	ft_has_pos_arg(const char *format, int i);
 int	ft_has_flags(const char *format, int i);
 int	ft_has_min_fieldwidth(const char *format, int i);
 int	ft_has_precision(const char *format, int i);
+int	ft_has_lengthmod(const char *format, int i);
 
 #endif
