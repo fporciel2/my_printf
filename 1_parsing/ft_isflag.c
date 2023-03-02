a49d1936aaaaef5a8249e5bb2541ecc1bbb7df9f
Federico Porciello <fporciel@e4r7p4.42roma.it>
2023-03-02 16:49:08 +0100

    Update.

--- 1_parsing/ft_isflag.c
+++ 1_parsing/ft_isflag.c
@@ -6,7 +6,7 @@
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 14:33:53 by fporciel          #+#    #+#             */
-/*   Updated: 2023/03/02 15:09:24 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:48:13 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 int	ft_isflag(const char formati)
 {
-	if ((formati == 35) || (formati == 48) || 
+	if ((formati == 35) || (formati == 48) || (formati == 45)
+			|| (formati == 32) || (formati == 43))
+		return (1);
+	else
+		return (0);
 }
